cbf64306d668cf244dfa3bf80094ddb6028731db
Zhaochen <zhaochen01@juran.com.cn>
2018-08-15 09:32:57 +0800

    脚手架搭建

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,11 +35,15 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       '@': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'base': resolve('src/base'),
+      'api': resolve('src/api')
     }
   },
   module: {
     rules: [
-      ...(config.dev.useEslint ? [createLintingRule()] : []),
+     // ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
         loader: 'vue-loader',
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
+
+// Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
